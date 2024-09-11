5436ae20f2e58622d52dd638c2da81133ea8891d
ankit <ap8424856@gmail.com>
2024-09-11 14:59:17 +0530

    added cors

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./config/conn");
-const cors = require("cors")
-const { prepareDevMode } = require("./config/prepare");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 if (process.env.NODE_ENV == "dev") {
+	const { prepareDevMode } = require("./config/prepare");
 	prepareDevMode();
 	app.use("/assets/thumbnails", express.static("assets/thumbnails"));
 	const morgan = require("morgan");
