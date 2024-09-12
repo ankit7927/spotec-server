900a53b887b7ba45c0d0076762d00bb67897a7d7
ankit <ap8424856@gmail.com>
2024-09-12 14:40:26 +0530

    added image not found for unavailable thumbnails

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/public", express.static("public"));
 
 if (process.env.NODE_ENV == "dev") {
 	const { prepareDevMode } = require("./config/prepare");
