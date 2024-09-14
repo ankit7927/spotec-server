76952c480775d3fbb3dc57ea576f61a8c69526f4
ankit <ap8424856@gmail.com>
2024-09-14 22:08:08 +0530

    modified dev preparer

--- src/config/prepare.js
+++ src/config/prepare.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 
 const prepareDevMode = async () => {
 	if (!fs.existsSync("assets")) fs.mkdirSync("assets");
-	if (!fs.existsSync("assets/audios")) fs.mkdirSync("assets/audios");
-	if (!fs.existsSync("assets/thumbnails")) fs.mkdirSync("assets/thumbnails");
 };
 
 module.exports = { prepareDevMode };
